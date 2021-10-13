671743b398797eafb2e1b7d95609e987714028c2
George Litos <giorgoslytos@gmail.com>
2021-10-13 17:31:37 +0300

    OOP implementation

--- script.js
+++ script.js
@@ -1,118 +1,178 @@
-const getCard = (name, image, status, species) => `<div class="card">
-<img src="${image}" class="card-img"><div class="info">
-<div class="name">${name}</div>
-  <div class="status ${status}">${status} ${species}</div>
-</div>
-</div>`;
+if ($('.main-content')) {
+  const renderCard = (name, image, status, species) => `
+    <div class="card">
+      <img src="${image}" class="card-img">
+      <div class="info">
+        <div class="name">${name}</div>
+        <div class="status ${status}">${status} ${species}</div>
+      </div>
+    </div>
+  `;
 
-const content = $(".api-content");
-const exitModal = $(".modal");
-const chevrones = $(".chevron");
+  const renderCharacter = ({
+    image,
+    name,
+    status,
+    species,
+    gender,
+    location,
+    episode,
+  }) => `
+    <div class="modal-content">
+    <span class="close">&times;</span>
+    <img class="modal-img" src="${image}"/>
+    <p class="modal-name"><span>${name}</span></p>
+    <p class="modal-status ${status}"><span>${status}</span> -</p>
+    <p class="modal-species">${species}</p>
+    <div class="modal-footer">
+      <div class="modal-gender">Gender: 
+        <img class="gender-symbol" src=${
+          gender === 'Male'
+            ? './images/mars-solid.svg'
+            : './images/venus-solid.svg'
+        } ></img> <span>${gender}</span></div>
+      <div class="modal-location">Last seen location: <span>${
+        location.name
+      }</span></div>
+      <div class="modal-episode">
+        Number of episodes appeared: <span>${episode.length}</span>
+      </div>
+    </div>
+    </div>
+  `;
 
-const homePage = {
-  page: 1,
-  totalPages: 1,
-};
+  const baseUrl = 'https://rickandmortyapi.com/api/character'
 
-const clickOnCharHandler = (event) => {
-  console.log(event.data.id);
-  $(".modal").show();
-  getCharacter(event.data.id);
-};
+  const loader = () => `
+    <div class="modal-content">
+      <div class="loader"></div>
+    </div>
+  `;
+  const $content = $('.api-content');
+  const $modal = $('.modal');
+  const $chevrones = $('.chevron');
 
-exitModal.click(() => {
-  clearModal();
-  $(".modal").hide();
-});
-
-chevrones.click((el) => {
-  console.log(el.currentTarget.classList.value);
-  switch (el.currentTarget.classList.value) {
-    case "chevron next":
-      if (homePage.page >= homePage.totalPages) {
-        return;
-      }
-      $(".api-content").empty();
-      homePage.page++;
-      getPage(homePage.page);
-      break;
-    case "chevron prev":
-      if (homePage.page <= 1) {
-        return;
-      }
-      $(".api-content").empty();
-      homePage.page--;
-      getPage(homePage.page);
-  }
-});
-
-export const getPage = (id) =>
-  fetch(`https://rickandmortyapi.com/api/character/?page=${id}`)
-    .then((res) => res.json())
-    .then((data) => [data.results, data.info])
-    .then(([chars, info]) => {
-      chars.forEach(({ id, name, image, status, species }) =>
-        content.append(
-          $(getCard(name, image, status, species)).click(
-            { id },
-            clickOnCharHandler
-          )
-        )
-      );
-      window.history.pushState("nextPage", "page", `/?page=${id}`);
-      return info;
-    })
-    .then(({ pages }) => {
-      homePage.totalPages = pages;
-      homePage.page = +id;
-      $(".pages").text(`${homePage.page} of ${homePage.totalPages}`);
-      switch (homePage.page) {
-        case 1:
-          $(".chevron.prev").addClass("disabled");
-          break;
-        case homePage.totalPages:
-          $(".chevron.next").addClass("disabled");
-          break;
-        default:
-          $(".chevron.next").removeClass("disabled");
-          $(".chevron.prev").removeClass("disabled");
-      }
-    });
-
-const getCharacter = (id) =>
-  fetch(`https://rickandmortyapi.com/api/character/${id}`).then((res) =>
-    res
-      .json()
-      .then(({ image, name, status, species, gender, location, episode }) => {
-        $(".modal-img").attr("src", image);
-        $(".modal-name span").text(name);
-        $(".modal-status span").text(status);
-        $(".modal-species").text(species);
-        $(".modal-status").attr("class", `modal-status ${status}`);
-        $(".modal-gender span").text(gender);
-        if (gender === "Male") {
-          $(".modal-gender img").attr("src", "./images/mars-solid.svg");
+  function App() {
+    return {
+      state: {
+        page: 1,
+        totalPages: null,
+        character: null,
+      },
+      fetchURLParams: function () {
+        if (window.location.search) {
+          const page = Number(
+            new URLSearchParams(window.location.search).get('page')
+          );
+          !isNaN(page) ? (this.state.page = page) : (this.state.page = 1);
+          this.state.character = Number(
+            new URLSearchParams(window.location.search).get('character')
+          );
+          if (this.state.page < 1) {
+            this.state.page = 1;
+          }
         } else {
-          $(".modal-gender img").attr("src", "./images/venus-solid.svg");
+          this.state.page = 1;
         }
-        $(".modal-location span").text(location.name);
-        $(".modal-episode span").text(episode.length);
-      })
-  );
+        this.getPageCharacters(this.state.page);
+        if (this.state.character > 0) {
+          $('.modal').show();
+          this.getCharacter(this.state.character);
+        }
+      },
+      getPageCharacters: function (id) {
+        $content.append(loader);
+        fetch(`${baseUrl}/?page=${id}`)
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.error === 'There is nothing here') {
+              this.getPageCharacters(1);
+              throw new Error(res.error);
+            }
+            return res;
+          })
+          .then((data) => [data.results, data.info])
+          .then(([chars, info]) => {
+            $content.empty();
+            chars.forEach(({ id, name, image, status, species }) =>
+              $content.append(
+                $(renderCard(name, image, status, species)).click(
+                  { id },
+                  this.characterHandler.bind(this)
+                )
+              )
+            );
+            window.history.pushState('nextPage', 'page', `/?page=${id}`);
+            return info;
+          })
+          .then(({ pages }) => {
+            this.state.totalPages = pages;
+            this.state.page = +id;
+            $('.pages').text(`${this.state.page} of ${this.state.totalPages}`);
+            switch (this.state.page) {
+              case 1:
+                $('.chevron.prev').addClass('disabled');
+                break;
+              case this.state.totalPages:
+                $('.chevron.next').addClass('disabled');
+                break;
+              default:
+                $('.chevron.next').removeClass('disabled');
+                $('.chevron.prev').removeClass('disabled');
+            }
+          });
+      },
+      getCharacter: function (id) {
+        $modal.append(loader);
+        fetch(`${baseUrl}/${id}`).then((res) =>
+          res.json().then((character) => {
+            $modal.empty();
+            $modal.append(renderCharacter(character));
+            window.history.pushState(
+              'character-view',
+              'character-view',
+              `/?page=${this.state.page}&character=${id}`
+            );
+          })
+        );
+      },
+      chevronHandler: function (event) {
+        switch (event.currentTarget.classList.value) {
+          case 'chevron next':
+            if (this.state.page >= this.state.totalPages) {
+              return;
+            }
+            $('.api-content').empty();
+            this.state.page++;
+            this.getPageCharacters(this.state.page);
+            break;
+          case 'chevron prev':
+            if (this.state.page <= 1) {
+              return;
+            }
+            $('.api-content').empty();
+            this.state.page--;
+            this.getPageCharacters(this.state.page);
+        }
+      },
+      characterHandler: function (event) {
+        $('.modal').show();
+        this.getCharacter(event.data.id);
+      },
+      clearModal: function () {
+        $modal.empty();
+        window.history.pushState(
+          'nextPage',
+          'page',
+          `/?page=${this.state.page}`
+        );
+        $('.modal').hide();
+      },
+    };
+  }
 
-const clearModal = () => {
-  $(".modal-img").attr("src", "");
-  $(".modal-name span").text("");
-  $(".modal-status span").text("");
-  $(".modal-species").text("");
-  $(".modal-status").attr("class", `modal-status`);
-  $(".modal-gender span").text("");
-  $(".modal-location span").text("");
-  $(".modal-episode span").text("");
-};
-
-if (window.location.search) {
-  homePage.page = new URLSearchParams(window.location.search).get("page");
+  const app = new App();
+  $chevrones.click(app.chevronHandler.bind(app));
+  $modal.click(app.clearModal.bind(app));
+  app.fetchURLParams();
 }
-
-getPage(homePage.page);
